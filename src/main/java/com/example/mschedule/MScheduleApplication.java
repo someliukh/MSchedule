673c08d2c0e369677ca038900b4cfe1f519e1985
Farid545 <aslanovfarid04@gmail.com>
2024-04-02 00:52:12 +0300

    Init commit

---
+++ src/main/java/com/example/mschedule/MScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MScheduleApplication.class, args);
+    }
+
+}
