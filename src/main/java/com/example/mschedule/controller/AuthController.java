b09789f241a4fa46b6300386f864e84051422736
someliukh <stepan.omeliukh.work@gmail.com>
2024-04-11 19:39:58 +0300

    Add CrossOrigin to comunicate with FE

--- src/main/java/com/example/mschedule/controller/AuthController.java
+++ src/main/java/com/example/mschedule/controller/AuthController.java
@@ -8,10 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
@@ -19,6 +16,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 public class AuthController {
 
   private final AuthServiceImpl service;
--- src/main/java/com/example/mschedule/controller/UserController.java
+++ src/main/java/com/example/mschedule/controller/UserController.java
@@ -18,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/staff")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 public class UserController {
 
     private final UserService userService;
